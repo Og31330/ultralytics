0b571b217a73505fb120df4ccdc3e9f0be9c089a
Og31330 <78754449+Og31330@users.noreply.github.com>
2024-03-10 15:24:47 +0100

    Update loss.py
    Back to standard L1 loss such as described in 
    https://github.com/ultralytics/ultralytics/issues/2543
    
    for better keypoint detection

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -136,13 +136,17 @@ class KeypointLoss(nn.Module):
         self.sigmas = sigmas
 
     def forward(self, pred_kpts, gt_kpts, kpt_mask, area):
-        """Calculates keypoint loss factor and Euclidean distance loss for predicted and actual keypoints."""
+        """Calculates keypoint loss factor and Euclidean distance loss for predicted and actual keypoints.
         d = (pred_kpts[..., 0] - gt_kpts[..., 0]).pow(2) + (pred_kpts[..., 1] - gt_kpts[..., 1]).pow(2)
         kpt_loss_factor = kpt_mask.shape[1] / (torch.sum(kpt_mask != 0, dim=1) + 1e-9)
         # e = d / (2 * (area * self.sigmas) ** 2 + 1e-9)  # from formula
         e = d / (2 * self.sigmas).pow(2) / (area + 1e-9) / 2  # from cocoeval
-        return (kpt_loss_factor.view(-1, 1) * ((1 - torch.exp(-e)) * kpt_mask)).mean()
+        return (kpt_loss_factor.view(-1, 1) * ((1 - torch.exp(-e)) * kpt_mask)).mean()"""
 
+        d = (pred_kpts[..., 0] - gt_kpts[..., 0]).abs() + (pred_kpts[..., 1] - gt_kpts[..., 1]).abs()
+        kpt_loss_factor = (torch.sum(kpt_mask != 0) + torch.sum(kpt_mask == 0)) / (torch.sum(kpt_mask != 0) + 1e-9)
+        return kpt_loss_factor * (d * kpt_mask).mean()
+        
 
 class v8DetectionLoss:
     """Criterion class for computing training losses."""
